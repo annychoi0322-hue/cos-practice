0decad6b307980b54dd1766a27b1658dbb328958
annychoi0322-hue <annychoi0322@kentech.ac.kr>
2026-06-06 04:01:11 +0000

    update

--- cos-project2-main/edge/process_manager.cpp
+++ cos-project2-main/edge/process_manager.cpp
@@ -7,6 +7,12 @@
 #include <ctime>
 using namespace std;
 
+// ── 버전 선택 (하나만 주석 해제) ──
+// #define PROCESS_VERSION_1
+// #define PROCESS_VERSION_2
+#define PROCESS_VERSION_3
+// ──────────────────────────────────
+
 ProcessManager::ProcessManager()
 {
   this->num = 0;
@@ -16,26 +22,20 @@ void ProcessManager::init()
 {
 }
 
-// TODO: You should implement this function if you want to change the result of the aggregation
 uint8_t *ProcessManager::processData(DataSet *ds, int *dlen)
 {
   uint8_t *ret, *p;
-  int num, len;
+  int num, tmp;
   HouseData *house;
-  Info *info;
   TemperatureData *tdata;
   HumidityData *hdata;
   PowerData *pdata;
-  char buf[BUFLEN];
-  ret = (uint8_t *)malloc(BUFLEN);
-  int tmp, min_humid, min_temp, min_power, month;
   double sum_power;
   time_t ts;
-  struct tm *tm;
+  struct tm *tm_info;
+  int month;
 
   ret = (uint8_t *)malloc(BUFLEN);
-
-
 
   num = ds->getNumHouseData();
   sum_power = 0;
@@ -48,71 +48,38 @@ uint8_t *ProcessManager::processData(DataSet *ds, int *dlen)
   tdata = ds->getTemperatureData();
   hdata = ds->getHumidityData();
 
-  ::memset(ret, 0, BUFLEN);
-  p = ret;
-
-  tmp = 1; VAR_TO_MEM_1BYTE(tmp, p);                        // version = 1
-
-  tmp = (int)(sum_power / num);
-  VAR_TO_MEM_2BYTES_BIG_ENDIAN(tmp, p);                     // avg_power (2B)
-
-  tmp = (int)tdata->getValue(); VAR_TO_MEM_1BYTE(tmp, p);   // avg_temp  (1B)
-  tmp = (int)hdata->getValue(); VAR_TO_MEM_1BYTE(tmp, p);   // avg_humid (1B)
-
-  *dlen = p - ret;  // = 5
-  return ret;
-}
-
-
-  // Example) getting the month value from the timestamp
   ts = ds->getTimestamp();
-  tm = localtime(&ts);
-  month = tm->tm_mon + 1;
+  tm_info = localtime(&ts);
+  month = tm_info->tm_mon + 1;
 
-  // Example) initializing the memory to send to the network manager
   memset(ret, 0, BUFLEN);
-  *dlen = 0;
   p = ret;
 
-  // Example) saving the values in the memory
 #if defined(PROCESS_VERSION_1)
-  // v1: avg_temp + avg_humid + avg_power + month
-  tmp = (int)tdata->getValue();        
-  VAR_TO_MEM_1BYTE_BIG_ENDIAN(tmp, p);
-  tmp = (int)hdata->getValue();        
-  VAR_TO_MEM_1BYTE_BIG_ENDIAN(tmp, p);
-  tmp = (int)(sum_power / num);        
-  VAR_TO_MEM_2BYTES_BIG_ENDIAN(tmp, p);
-  tmp = month;                         
-  VAR_TO_MEM_1BYTE_BIG_ENDIAN(tmp, p);
+  // v1: avg_temp(1B) + avg_humid(1B) + avg_power(2B) + month(1B)
+  tmp = (int)tdata->getValue();     VAR_TO_MEM_1BYTE_BIG_ENDIAN(tmp, p);
+  tmp = (int)hdata->getValue();     VAR_TO_MEM_1BYTE_BIG_ENDIAN(tmp, p);
+  tmp = (int)(sum_power / num);     VAR_TO_MEM_2BYTES_BIG_ENDIAN(tmp, p);
+  tmp = month;                      VAR_TO_MEM_1BYTE_BIG_ENDIAN(tmp, p);
 
 #elif defined(PROCESS_VERSION_2)
-  // v2: max_temp + min_temp + avg_power + month
-  tmp = (int)tdata->getMax();          
-  VAR_TO_MEM_1BYTE_BIG_ENDIAN(tmp, p);
-  tmp = (int)tdata->getMin();          
-  VAR_TO_MEM_1BYTE_BIG_ENDIAN(tmp, p);
-  tmp = (int)(sum_power / num);        
-  VAR_TO_MEM_2BYTES_BIG_ENDIAN(tmp, p);
-  tmp = month;                         
-  VAR_TO_MEM_1BYTE_BIG_ENDIAN(tmp, p);
+  // v2: max_temp(1B) + min_temp(1B) + avg_power(2B) + month(1B)
+  tmp = (int)tdata->getMax();       VAR_TO_MEM_1BYTE_BIG_ENDIAN(tmp, p);
+  tmp = (int)tdata->getMin();       VAR_TO_MEM_1BYTE_BIG_ENDIAN(tmp, p);
+  tmp = (int)(sum_power / num);     VAR_TO_MEM_2BYTES_BIG_ENDIAN(tmp, p);
+  tmp = month;                      VAR_TO_MEM_1BYTE_BIG_ENDIAN(tmp, p);
 
 #elif defined(PROCESS_VERSION_3)
-  // v3: max_humid + min_humid + avg_power + month
-  tmp = (int)hdata->getMax();          
-  VAR_TO_MEM_1BYTE_BIG_ENDIAN(tmp, p);
-  tmp = (int)hdata->getMin();          
-  VAR_TO_MEM_1BYTE_BIG_ENDIAN(tmp, p);
-  tmp = (int)(sum_power / num);        
-  VAR_TO_MEM_2BYTES_BIG_ENDIAN(tmp, p);
-  tmp = month;                         
-  VAR_TO_MEM_1BYTE_BIG_ENDIAN(tmp, p);
+  // v3: max_humid(1B) + min_humid(1B) + avg_power(2B) + month(1B)
+  tmp = (int)hdata->getMax();       VAR_TO_MEM_1BYTE_BIG_ENDIAN(tmp, p);
+  tmp = (int)hdata->getMin();       VAR_TO_MEM_1BYTE_BIG_ENDIAN(tmp, p);
+  tmp = (int)(sum_power / num);     VAR_TO_MEM_2BYTES_BIG_ENDIAN(tmp, p);
+  tmp = month;                      VAR_TO_MEM_1BYTE_BIG_ENDIAN(tmp, p);
 
 #else
- #error "PROCESS_VERSION not defined."
+  #error "PROCESS_VERSION not defined."
 #endif
 
-  *dlen = p - ret;  // = 5
+  *dlen = p - ret;
   return ret;
 }
-
