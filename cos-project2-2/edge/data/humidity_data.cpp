7b1866503f5ce178c807c2e891fe26b4ad557643
annychoi0322-hue <annychoi0322@kentech.ac.kr>
2026-06-07 01:47:41 +0900

    Update humidity_data.cpp

--- cos-project2-2/edge/data/humidity_data.cpp
+++ cos-project2-2/edge/data/humidity_data.cpp
@@ -7,10 +7,10 @@
 HumidityData::HumidityData(time_t timestamp, double min, double max, double avg)
   : SensorData(timestamp, min, max, avg)
 {
-  this->next = NULL;
-  this->unit = "%";
+  this->next = NULL; // 다음 노드 없음
+  this->unit = "%";  // 습도 단위 설정
 }
 
-void HumidityData::setNext(HumidityData *data) { this->next = data; }
-HumidityData *HumidityData::getNext()          { return this->next; }
-string HumidityData::getUnit()                 { return this->unit; }
+void HumidityData::setNext(HumidityData *data) { this->next = data; }  // 다음 HumidityData 노드 설정
+HumidityData *HumidityData::getNext()          { return this->next; }  // 다음 HumidityData 노드 반환
+string HumidityData::getUnit()                 { return this->unit; }  // 습도 단위 반환
