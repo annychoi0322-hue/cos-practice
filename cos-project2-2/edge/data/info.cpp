a103ba3a39e61bc8c962e8864812d5480dd17771
annychoi0322-hue <annychoi0322@kentech.ac.kr>
2026-06-07 01:43:44 +0900

    Update info.cpp

--- cos-project2-2/edge/data/info.cpp
+++ cos-project2-2/edge/data/info.cpp
@@ -7,31 +7,31 @@
 #include <iomanip>
 using namespace std;
 
-Info::Info(int num)
+Info::Info(int num)                                 // 랜덤 정보 생성 생성자
 {
-  this->serial = generateRandomSerial(num);
+  this->serial = generateRandomSerial(num);         // num 값을 기반으로 serial / name / address를 랜덤 생성
   this->name = generateRandomName(num);
   this->address = generateRandomAddress(num);
 }
 
-Info::Info(string serial, string name, string address)
+Info::Info(string serial, string name, string address)  // 직접 값 입력 생성자
 {
   this->serial = serial;
   this->name = name;
   this->address = address;
 }
 
-void Info::setSerial(string serial)
+void Info::setSerial(string serial)  // serial setter
 {
   this->serial = serial;
 }
 
-string Info::getSerial()
+string Info::getSerial()  // serial getter
 {
   return this->serial;
 }
 
-string Info::generateRandomSerial(int num)
+string Info::generateRandomSerial(int num)  // 랜덤 serial 생성 (11자리 영문/숫자 혼합 문자열 생성, 뒤에 num을 5자리 zero-padding으로 붙임)
 {
   int coin, v;
   stringstream ss;
@@ -42,32 +42,32 @@ string Info::generateRandomSerial(int num)
   srand(seed);
   ret = "";
 
-  for (int i=0; i<11; i++)
+  for (int i=0; i<11; i++)            // 11자리 랜덤 문자열 생성
   {
     coin = rand() % 2;
     if (!coin)
-      v = rand() % 10 + '0';
+      v = rand() % 10 + '0';  // 숫자
     else
-      v = rand() % 26 + 'A';
-    ret += v;
+      v = rand() % 26 + 'A';  // 대문자
+    ret += v;                
   }
-  ss << setw(5) << setfill('0') << num;
+  ss << setw(5) << setfill('0') << num;   // num을 5자리로 padding해서 뒤에 붙임
   ret += ss.str();
 
   return ret;
 }
 
-void Info::setName(string name)
+void Info::setName(string name)   // name setter
 {
   this->name = name;
 }
 
-string Info::getName()
+string Info::getName()            // name getter
 {
   return this->name;
 }
 
-string Info::generateRandomName(int num)
+string Info::generateRandomName(int num)    // 랜덤 이름 생성, first name + last name 조합
 {
   string ret;
   int count_first, count_last, first_index, last_index;
@@ -86,17 +86,17 @@ string Info::generateRandomName(int num)
   return ret;
 }
 
-void Info::setAddress(string address)
+void Info::setAddress(string address)      // address setter
 {
   this->address = address;
 }
 
-string Info::getAddress()
+string Info::getAddress()                  // address getter
 {
   return this->address;
 }
 
-string Info::generateRandomAddress(int num)
+string Info::generateRandomAddress(int num)   // 랜덤 주소 생성, 1~5000 번지 + 고정 도로명 주소
 {
   string ret;
   int addr, count;
