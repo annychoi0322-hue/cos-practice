b9dfd3b0e29404a6cd6698ba48481e3de7302d34
annychoi0322-hue <annychoi0322@kentech.ac.kr>
2026-06-07 01:52:57 +0900

    Update house_data.h

--- cos-project2-2/edge/data/house_data.h
+++ cos-project2-2/edge/data/house_data.h
@@ -8,27 +8,31 @@
 #include "info.h"
 using namespace std;
 
+// HouseData zmffotm
+// 하나의 house 데이터를 표현하는 클래스
+// 포함 구성: Info 객체, PowerData, next
+// 전체 구조에서 HouseData는 "노드 역할"을 수행
 class HouseData 
 {
   private:
-    Info *info;
-    PowerData *data;
-    HouseData *next;
+    Info *info;               // 해당 House의 기본 정보
+    PowerData *data;          // 해당 House의 전력 데이터
+    HouseData *next;          // Linked List에서 다음 HouseData
   public:
-    HouseData(Info *info);
-    HouseData(Info *info, PowerData *power);
-    void setInfo(Info *info);
-    Info *getInfo();
+    HouseData(Info *info);                      // Info만 포함된 HouseData 생성자
+    HouseData(Info *info, PowerData *power);    // Info + PowerData 포함 생성자
+    void setInfo(Info *info);                   // Info 객체 설정
+    Info *getInfo();                            // Info 객체 반환
 
-    string getSerial();
-    string getName();
-    string getAddress();
+    string getSerial();                         // serial 값 반환
+    string getName();                           // name 값 반환
+    string getAddress();                        // address 값 반환
 
-    void setPowerData(PowerData *power);
-    PowerData *getPowerData();
+    void setPowerData(PowerData *power);        // PowerData 성정
+    PowerData *getPowerData();                  // PowerData 반환
 
-    void setNext(HouseData *hd);
-    HouseData *getNext();
+    void setNext(HouseData *hd);                // 다음 HouseData 서정
+    HouseData *getNext();                       // 다음 HouseData 반환
 };
 
 #endif /* __HOUSE_DATA_H__ */
