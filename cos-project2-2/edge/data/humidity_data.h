3b84420875860516c8a4970d3b06725ab64d06e3
annychoi0322-hue <annychoi0322@kentech.ac.kr>
2026-06-07 01:46:18 +0900

    Update humidity_data.h

--- cos-project2-2/edge/data/humidity_data.h
+++ cos-project2-2/edge/data/humidity_data.h
@@ -13,15 +13,15 @@
 class HumidityData : public SensorData
 {
   private:
-    HumidityData *next;
+    HumidityData *next;      // Linked List에서 다음 HumidityData를 가리키는 포인터
 
   public:
-    HumidityData(time_t timestamp, double min, double max, double avg);
+    HumidityData(time_t timestamp, double min, double max, double avg);  // HumidityData 생성자
 
-    void setNext(HumidityData *data);
-    HumidityData *getNext();
+    void setNext(HumidityData *data);  // 다음 노드 설정
+    HumidityData *getNext();           // 다음 노드 반환
 
-    string getUnit();
+    string getUnit();                  // 습도 단위 반환
 };
 
 #endif /* __HUMIDITY_DATA_H__ */
